8f97072bbcf1f86c1e448688e4892d6233f6a724
Joe <jowood4@gmail.com>
2014-12-25 15:02:53 -0500

    Added UDP files

---
+++ udp_write.py
@@ -0,0 +1,15 @@
+import socket
+
+#UDP_IP = "127.0.0.1"
+#UDP_IP = "192.168.1.243"
+UDP_IP = "192.168.240.1"
+UDP_PORT = 49002
+MESSAGE = "Hello, World!"
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
